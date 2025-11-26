45de6c0dcf08a639b609fa60c343d07c55de26ab
Piero Funes <pierofl2005@gmail.com>
2025-11-26 13:30:39 +0100

    1

---
+++ Clases/Segundo Trimestre/006/004-composicion.sql
@@ -0,0 +1,3 @@
+SELECT * FROM matriculas
+LEFT JOIN asignaturas
+ON matriculas.id_asignatura = asignaturas.Identificador;
---
+++ Clases/Segundo Trimestre/006/010-creamos un usuario.sql
@@ -0,0 +1,21 @@
+
+CREATE USER 
+'composiciones'@'localhost' 
+IDENTIFIED  BY 'composiciones123';
+
+GRANT USAGE ON *.* TO 'composiciones'@'localhost';
+
+
+ALTER USER 'composiciones'@'localhost ' 
+REQUIRE NONE 
+WITH MAX_QUERIES_PER_HOUR 0 
+MAX_CONNECTIONS_PER_HOUR 0 
+MAX_UPDATES_PER_HOUR 0 
+MAX_USER_CONNECTIONS 0;
+
+-- dale acceso a la base de datos composiciones
+GRANT ALL PRIVILEGES ON composiciones.* 
+TO 'composiciones'@'localhost';
+
+-- recarga la tabla de privilegios
+FLUSH PRIVILEGES;
