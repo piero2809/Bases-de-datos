6b240c969fe90806e91b251cbc9e024c763cd38e
Piero Funes <pierofl2005@gmail.com>
2025-11-26 13:48:38 +0100

    1

--- Clases/Segundo Trimestre/006/011-plantilla de conexion.py
+++ Clases/Segundo Trimestre/006/011-plantilla de conexion.py
@@ -1,15 +1,16 @@
 import mysql.connector 
 
+
 conexion = mysql.connector.connect(
   host="localhost",
   user="composiciones",
   password="composiciones123",
   database="composiciones"
-)                                      
+)                         
   
 cursor = conexion.cursor() 
 cursor.execute("SELECT * FROM matriculas_join;")  
 
 filas = cursor.fetchall()
 
-print(filas)
+print(filas)   
