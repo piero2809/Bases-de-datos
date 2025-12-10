d23c02dfdd578b745643787a044dabc24c010a84
Piero Funes <pierofl2005@gmail.com>
2025-12-10 14:07:19 +0100

    1

--- Clases/Segundo Trimestre/008-Proyects/003-Panel de control/101-Ejercicios/aplicacion/admin/escritorio.php
+++ Clases/Segundo Trimestre/008-Proyects/003-Panel de control/101-Ejercicios/aplicacion/admin/escritorio.php
@@ -1,1 +1,19 @@
-Si estas viendo esto, es que has conseguido hacer login ok
+<!doctype html>
+<html lang="es">
+
+<head>
+    <title>El jocarsa - Panel de control</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+
+<body>
+    <nav>
+        <button>Noticias</button>
+        <button>Autores</button>
+    </nav>
+    <main>
+    </main>
+</body>
+
+</html>
