ca81c5e65a2a24534d95aefd787a1d0bbef76f1c
piero287 <pierofl2005@gmail.com>
2025-12-17 08:46:16 +0100

    1

---
+++ Clases/Segundo Trimestre/008-Proyects/004-Supercrud/002-Vomito el array.php
@@ -0,0 +1,26 @@
+<table>
+    <?php
+    $host = "localhost";
+    $user = "tiendaonlinedamdaw";
+    $pass = "Tiendaonlinedamdaw123$";
+    $db = "tiendaonlinedamdaw";
+
+    $conexion = new mysqli($host, $user, $pass, $db);
+    $resultado = $conexion->query("
+  	SELECT * FROM cliente;
+  ");
+    while ($fila = $resultado->fetch_assoc()) {
+        var_dump($fila);
+        echo "<br><br>";
+        echo '
+    <tr>
+    	<td>' . $fila['nombre_cliente'] . '</td>
+      <td>' . $fila['apellidos'] . '</td>
+      <td>' . $fila['email'] . '</td>
+      <td>' . $fila['direccion'] . '</td>
+      <td>' . $fila['telefono'] . '</td>
+    </tr>
+    ';
+    }
+    ?>
+</table>
